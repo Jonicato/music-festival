5eec62251c841c5fb5b3744c86f1057c6c7d2a4d
Jonathan Rodriguez <jhonarodriguez98@gmail.com>
2022-11-09 23:41:38 -0600

    Primeros estilos del header

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 
     // Los pipes son acciones que se ejecutan una tras otra
     src('src/scss/**/*.scss') //Identificar el archivo SASS
+        .pipe(plumber())
         .pipe(sass()) // Compilarlo
         .pipe(dest('build/css')); // Almacenarlo en el disco duro
 
